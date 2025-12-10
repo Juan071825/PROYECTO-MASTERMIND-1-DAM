58122f0a3bc9ea15fb9db04b3ce30d0bb8fc5642
juan071825 <juanmateoalvarez@gmail.com>
2025-12-10 13:47:22 +0100

    refactor(src/generar_poblacion_aleatoria.py): :fire: Eliminar printado en terminal de la población de soluciones
    Eliminar el printado en terminal de la población de soluciones en la funcion generar_poblacion_aleatoria

--- src/generar_poblacion_aleatoria.py
+++ src/generar_poblacion_aleatoria.py
@@ -2,7 +2,7 @@ from generar_codigo_aleatorio import generar_codigo_aleatorio
 
 def generar_poblacion_aleatoria(numero_soluciones):
     censo ={}
-    cont = 1
+    cont = 0
     UNO = 1
 
     while cont < numero_soluciones:
@@ -12,4 +12,5 @@ def generar_poblacion_aleatoria(numero_soluciones):
 
     return censo
 
-generar_poblacion_aleatoria(2)
+
+
