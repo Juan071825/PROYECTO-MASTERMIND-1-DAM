24287b1931030475ef70f1a74f5d0e8eab45be5c
juan071825 <juanmateoalvarez@gmail.com>
2025-12-12 14:13:37 +0100

    feat(medir_fitness.py): :sparkles: Añadir medir_fitness.py para medir el fitness  del individuo
    Añadir medir_fitness.py para medir el fitness del individuo.

---
+++ src/medir_fitness.py
@@ -0,0 +1,22 @@
+from generar_poblacion_aleatoria import generar_poblacion_aleatoria
+from introducir_codigo import pedir_codigo_secreto
+
+
+def medir_fitness():
+    censo = generar_poblacion_aleatoria(5)
+    codigo_secreto = pedir_codigo_secreto()
+    censo_individuos = list(censo.values())
+    registro_fitness = []
+    
+    for individuo in censo_individuos:
+        fitness = 0
+        for color in individuo:
+            if color in codigo_secreto:
+                fitness += 1
+            else:
+                fitness += 0
+        registro_fitness.append(fitness)
+    return registro_fitness
+
+
+print(medir_fitness())
