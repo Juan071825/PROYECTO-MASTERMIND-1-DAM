a2d902a91857841394de91eb704ea344116e01b6
juan071825 <juanmateoalvarez@gmail.com>
2025-12-12 12:45:48 +0100

    feat(src/parametros_mastermind.py): :sparkles: Añadir módulo con los parametros de los códigos
    Añadir módulo con los parámetros que definen los códigos que introduce el usuario y la máquina

---
+++ src/parametros_mastermind.py
@@ -0,0 +1,12 @@
+COLORES_ELEGIBLES = ['verde', 'azul', 'amarillo', 'morado', 'negro', 'marrón']
+COLORES_INDICADORES = ['rojo', 'blanco']
+
+EMOJIS_COLORES = {
+'rojo': "\U0001F534",
+'verde': "\U0001F7E2",
+'azul': "\U0001F535",
+'amarillo': "\U0001F7E1",
+'morado': "\U0001F7E3",
+'negro': "\u26AB",
+'blanco': "\u26AA",
+'marrón': "\U0001F7E4"}
