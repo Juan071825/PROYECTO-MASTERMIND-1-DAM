1e7523a44b69b15fcc97276059d861857ea570a4
juan071825 <juanmateoalvarez@gmail.com>
2025-12-17 13:37:38 +0100

    Revert "feat(src/medir_fitness.py): :sparkles: Terminar y añadir una nueva función."
    This reverts commit b318ed11e871b2c2bf2f175340116e81583c77b9.

--- src/medir_fitness.py
+++ src/medir_fitness.py
@@ -1,14 +1,12 @@
 from src.generar_poblacion_aleatoria import generar_poblacion_aleatoria
 from src.introducir_codigo import pedir_codigo_secreto
- 
-censo = generar_poblacion_aleatoria()
-codigo_secreto = pedir_codigo_secreto()
-censo_individuos = list(censo.values())
+
 
 def medir_fitness():
-
-   
-    valores_fitness = []
+    censo = generar_poblacion_aleatoria(5)
+    codigo_secreto = pedir_codigo_secreto()
+    censo_individuos = list(censo.values())
+    registro_fitness = []
     
     for individuo in censo_individuos:
         fitness = 0
@@ -17,24 +15,5 @@ def medir_fitness():
                 fitness += 1
             else:
                 fitness += 0
-        valores_fitness.append(fitness)
-        return valores_fitness
-
-
-
-
-
-
-def registrar_fitness():
-    
-    valores_fitness = medir_fitness()
-    dict_fitness = {}
-    
-    for key in censo.keys():
-        for valores in list(zip(censo_individuos, valores_fitness)):
-            dict_fitness[key] = valores
-
-    return print(dict_fitness)
-
-
-registrar_fitness()
+        registro_fitness.append(fitness)
+    return registro_fitness
