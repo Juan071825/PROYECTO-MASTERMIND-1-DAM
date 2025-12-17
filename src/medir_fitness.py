b175e21f0362e04c95997fceedbd9166a3dd9ce5
juan071825 <juanmateoalvarez@gmail.com>
2025-12-17 13:40:56 +0100

    feat(src/medir_fitness.py): :sparkles: Terminar y añadir una nueva función.
    Terminar la función medir_fitness y añadir la nueva función registrar_fitness.

--- src/medir_fitness.py
+++ src/medir_fitness.py
@@ -1,12 +1,14 @@
 from src.generar_poblacion_aleatoria import generar_poblacion_aleatoria
 from src.introducir_codigo import pedir_codigo_secreto
-
+ 
+censo = generar_poblacion_aleatoria()
+codigo_secreto = pedir_codigo_secreto()
+censo_individuos = list(censo.values())
 
 def medir_fitness():
-    censo = generar_poblacion_aleatoria(5)
-    codigo_secreto = pedir_codigo_secreto()
-    censo_individuos = list(censo.values())
-    registro_fitness = []
+
+   
+    valores_fitness = []
     
     for individuo in censo_individuos:
         fitness = 0
@@ -15,5 +17,24 @@ def medir_fitness():
                 fitness += 1
             else:
                 fitness += 0
-        registro_fitness.append(fitness)
-    return registro_fitness
+        valores_fitness.append(fitness)
+        return valores_fitness
+
+
+
+
+
+
+def registrar_fitness():
+    
+    valores_fitness = medir_fitness()
+    dict_fitness = {}
+    
+    for key in censo.keys():
+        for valores in list(zip(censo_individuos, valores_fitness)):
+            dict_fitness[key] = valores
+
+    return print(dict_fitness)
+
+
+registrar_fitness()
