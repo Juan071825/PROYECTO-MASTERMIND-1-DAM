2e50f89f2e057f8fca832bfb38b899706cca90f0
juan071825 <juanmateoalvarez@gmail.com>
2025-12-18 10:33:24 +0100

    fix(src/medir_fitness): :bug: Modificar módulo para que se adapte a los parámetros.
    Modificar módulo para que se adapte a los parámetros del juego mastermind.

--- src/medir_fitness.py
+++ src/medir_fitness.py
@@ -9,14 +9,27 @@ def medir_fitness():
 
    
     valores_fitness = []
-    
+
     for individuo in censo_individuos:
         fitness = 0
-        for color in individuo:
-            if color in codigo_secreto:
-                fitness += 1
+        for index in list(range(len(individuo))):
+            codigo_secreto_copy = codigo_secreto.copy()
+            individuo_copy = individuo.copy()
+            if individuo[index] == codigo_secreto[index]:
+                fitness += 2
+                codigo_secreto_copy[index] = None
+                individuo_copy[index] = None
+
+                
+
             else:
-                fitness += 0
+                if individuo[index] in codigo_secreto_copy:
+                    fitness += 1
+                    codigo_secreto_copy[codigo_secreto_copy.index(index)] = None
+
+                else:
+                    fitness += 0
+        
         valores_fitness.append(fitness)
     return valores_fitness
 
@@ -28,7 +41,7 @@ def medir_fitness():
 def registrar_fitness():
     
     valores_fitness = medir_fitness()
-    dict_fitness = {}
+    
     values_dict = list(zip(censo_individuos, valores_fitness))
     return print(dict(zip(censo.keys(), values_dict)))
 
