52ca641b92caad4bd91418425592cf7688e27720
juan071825 <juanmateoalvarez@gmail.com>
2025-12-18 19:28:38 +0100

    fix(src/medir_fitness.py): :bug: Modificar el código para que mida el fitness correctamente.
    Modificar el código para que mida el fitness de cada individuo correctamente.

--- src/medir_fitness.py
+++ src/medir_fitness.py
@@ -7,28 +7,31 @@ censo_individuos = list(censo.values())
 
 def medir_fitness():
 
-   
     valores_fitness = []
-
+    
+    
     for individuo in censo_individuos:
+        codigo_secreto_copy = codigo_secreto.copy()
+        individuo_copy = individuo.copy()
         fitness = 0
-        for index in list(range(len(individuo))):
-            codigo_secreto_copy = codigo_secreto.copy()
-            individuo_copy = individuo.copy()
-            if individuo[index] == codigo_secreto[index]:
+        
+        for index in range(len(individuo)):
+           #Coincidencia exacta (color y posición).
+            if individuo_copy[index] == codigo_secreto_copy[index]:
                 fitness += 2
                 codigo_secreto_copy[index] = None
                 individuo_copy[index] = None
 
-                
+
+        for index in range(len(individuo)):
+            #Coincidencia parcial (color pero no posición).
+            if individuo_copy[index] is not None and individuo_copy[index] in codigo_secreto_copy:
+                fitness += 1
+                codigo_secreto_copy[codigo_secreto_copy.index(individuo_copy[index])] = None
+                individuo_copy[index] = None
 
             else:
-                if individuo[index] in codigo_secreto_copy:
-                    fitness += 1
-                    codigo_secreto_copy[codigo_secreto_copy.index(index)] = None
-
-                else:
-                    fitness += 0
+                fitness += 0
         
         valores_fitness.append(fitness)
     return valores_fitness
