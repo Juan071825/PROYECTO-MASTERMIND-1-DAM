218b832fd1c4389dc960ff460aa9f3ee588e365b
juan071825 <juanmateoalvarez@gmail.com>
2025-12-27 19:05:16 +0100

    style(src/crear_generacion): :zap: Modificar el nombre de los individuos.
    Modificar el nombre de los individuos de la nueva generación.

--- src/crear_generacion.py
+++ src/crear_generacion.py
@@ -1,10 +1,22 @@
 
 
 def crear_generacion(progenitores, hijos):
+    diccionario_padres_hijos = {}
+    diccionario_padres_hijos.update(progenitores)
+    diccionario_padres_hijos.update(hijos)
+
     nueva_generacion = {}
-    nueva_generacion.update(progenitores)
-    nueva_generacion.update(hijos)
+    contador_entradas = 0
+    for entrada in list(diccionario_padres_hijos.values()):
+        nueva_generacion['individuo' + str(contador_entradas)] = entrada
+        contador_entradas += 1 
+        
     return nueva_generacion
+
+
+
+
+
 
 
 
@@ -12,7 +24,7 @@ if __name__ == "__main__":
     from src.medir_fitness import medir_fitness
     from src.crear_offspring import crear_offspring
 
-    progenitores = medir_fitness
+    progenitores = medir_fitness()
     hijos = crear_offspring(progenitores)
 
     nueva_generacion = crear_generacion(progenitores, hijos)
