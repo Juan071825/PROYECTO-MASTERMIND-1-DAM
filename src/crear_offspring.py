7427cd7707596b99a030a5fa5e329e2b88680602
juan071825 <juanmateoalvarez@gmail.com>
2025-12-27 19:03:15 +0100

    feat: :sparkles: Añadir la función para mutar cromosomas.
    Añadir la función para mutar cromosomas y su constante de mutación.

--- src/crear_offspring.py
+++ src/crear_offspring.py
@@ -1,14 +1,14 @@
 import random
-from src.parametros_mastermind import ALELOS_ELEGIBLES, EMOJIS_ALELOS
+from src.parametros_mastermind import ALELOS_ELEGIBLES, EMOJIS_ALELOS, PROBABILIDAD_MUTACION
 from src.selector_padres import selector_padres
 
 def crear_offspring(progenitores):
 
     UNO = 1
 
-    
     lista_hijos = []
-    
+    diccionario_hijos = {}
+
     while len(lista_hijos) < 25:
 
         pareja_seleccionada = random.choices(
@@ -22,10 +22,19 @@ def crear_offspring(progenitores):
         punto_cruce = random.randint(1, 3)
 
         hijo = padre[:punto_cruce] + madre[punto_cruce:]
+        hijo = generar_mutacion(hijo, PROBABILIDAD_MUTACION)
         lista_hijos.append(hijo)
 
-        diccionario_hijos = {}
+
         for hijo in lista_hijos:
             diccionario_hijos['hijo' + str(lista_hijos.index(hijo) + UNO)] = hijo
 
     return diccionario_hijos
+
+def generar_mutacion(hijo, probabilidad_mutacion):
+
+    if random.random() < probabilidad_mutacion:
+        index_alelo_mutado = random.randint(0, 3)
+        nuevo_alelo = random.choice(ALELOS_ELEGIBLES)
+        hijo[index_alelo_mutado] = EMOJIS_ALELOS[nuevo_alelo]
+    return hijo
