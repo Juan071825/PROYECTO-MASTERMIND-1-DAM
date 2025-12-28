b2cce4ab01fb7450fdaa05fa17fd96abd7f02627
juan071825 <juanmateoalvarez@gmail.com>
2025-12-28 20:01:02 +0100

    fix(main): :bug: Adaptar main al resto de módulos.
    Adaptar main para que se relacione correctamente con el resto de módulos.

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ from src.crear_generacion import crear_generacion
 from src.selector_padres import selector_padres
 from src.generar_poblacion_aleatoria import generar_poblacion_aleatoria
 from src.introducir_codigo import pedir_codigo_secreto
+from src.parametros_mastermind import TAMAÑO_POBLACION
 
 
 def introducir_cromosoma_secreto():
+    # """
+# Pedimos el código secreto al usuario.
+# """
     cromosoma_secreto = pedir_codigo_secreto()
     return cromosoma_secreto
 
@@ -14,16 +18,10 @@ def introducir_cromosoma_secreto():
 
 
 def registro_generaciones(cromosoma_secreto):
-
 # """
-# Pedimos el código secreto al usuario.
+# Generamos la población inicial y dos variables que necesitaremos más adelante.
 # """
-   
     contador_generaciones = 0
-
-# """
-# Generamos la población inicial.
-# """
     poblacion = generar_poblacion_aleatoria(100)
     lista_mejores_candidatos = []
 
@@ -31,24 +29,20 @@ def registro_generaciones(cromosoma_secreto):
 # Comenzamos el ciclo de generaciones.
 # """
     while contador_generaciones < 14:
-        
-        if contador_generaciones == 0:
-            mejor_candidato = mejor_candidato_generacion(poblacion, cromosoma_secreto)
-            lista_mejores_candidatos.append(mejor_candidato)
-            contador_generaciones += 1
-        
-        else:
-            fitness_poblacion = medir_fitness(poblacion, cromosoma_secreto)
-            padres_seleccionados = selector_padres(fitness_poblacion)
-            hijos = crear_offspring(padres_seleccionados)
-            fitness_hijos = medir_fitness(hijos, cromosoma_secreto)
-            poblacion = crear_generacion(padres_seleccionados, fitness_hijos)
-            mejor_candidato = mejor_candidato_generacion(poblacion, cromosoma_secreto)
-            lista_mejores_candidatos.append(mejor_candidato)
-            contador_generaciones += 1
 
-        if lista_mejores_candidatos[-1][1][1] == 8:
+        mejor_candidato = mejor_candidato_generacion(poblacion, cromosoma_secreto)
+        lista_mejores_candidatos.append(mejor_candidato)
+
+        if mejor_candidato[1][1] == 16: # fitness máximo 4*4 = 16
             break
+
+        fitness_poblacion = medir_fitness(poblacion, cromosoma_secreto)
+        padres_seleccionados = selector_padres(fitness_poblacion)
+        hijos = crear_offspring(padres_seleccionados)
+        poblacion = crear_generacion(padres_seleccionados, hijos, TAMAÑO_POBLACION)
+
+        contador_generaciones += 1
+
 
     return print(lista_mejores_candidatos)
 
@@ -57,35 +51,12 @@ def registro_generaciones(cromosoma_secreto):
 
 def mejor_candidato_generacion(poblacion, cromosoma_secreto):
     fitness_poblacion = medir_fitness(poblacion, cromosoma_secreto)
-    fitness_ordenado = sorted(fitness_poblacion.items(), key=lambda x: x[1], reverse=True) # ordena la pobalción en base a su fitness.
+    fitness_ordenado = sorted(fitness_poblacion.items(), key=lambda x: x[1][1], reverse=True) # ordena la pobalción en base a su fitness.
     mejor_candidato = fitness_ordenado[0]
     return mejor_candidato
 
 
 
 
-
 if __name__ == "__main__":
     registro_generaciones(cromosoma_secreto=introducir_cromosoma_secreto())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
