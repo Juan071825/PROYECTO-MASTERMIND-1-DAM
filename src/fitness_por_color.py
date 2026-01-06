93b3b5b57f32b87d7a1776c06fe76e8be460faa8
adriceka <adrigonzalezgonzalez007@gmail.com>
2026-01-06 22:48:18 +0100

    feat: :sparkles: Añadir modulo
    Añadir modulo para calcular fitness acumulado por color

---
+++ src/fitness_por_color.py
@@ -0,0 +1,6 @@
+from src.medir_fitness import medir_fitness
+from src.parametros_mastermind import EMOJIS_ALELOS
+
+def fitness_por_color(poblacion, cromosoma_secreto):
+    colores = ["verde", "azul", "amarillo", "morado", "negro", "marrón"]
+
