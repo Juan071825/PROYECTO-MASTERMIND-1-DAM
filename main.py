6911a9a7a93b3fdbe5075231fa07436474a817c6
juan071825 <juanmateoalvarez@gmail.com>
2026-01-07 11:43:03 +0100

    feat(main): :zap: Modificar la salida al usuario para que sea más visual.
    Modificar la salida que ve el usuario para que sea más visual.

--- main.py
+++ main.py
@@ -9,42 +9,10 @@ from src.parametros_mastermind import TAMAÑO_POBLACION
 
 def introducir_cromosoma_secreto():
     # """
-# Pedimos el código secreto al usuario.
-# """
+    # Pedimos el código secreto al usuario.
+    # """
     cromosoma_secreto = pedir_codigo_secreto()
     return cromosoma_secreto
-
-
-
-
-def registro_generaciones(cromosoma_secreto):
-# """
-# Generamos la población inicial y dos variables que necesitaremos más adelante.
-# """
-    contador_generaciones = 0
-    poblacion = generar_poblacion_aleatoria(100)
-    lista_mejores_candidatos = []
-
-# """
-# Comenzamos el ciclo de generaciones.
-# """
-    while contador_generaciones < 14:
-
-        mejor_candidato = mejor_candidato_generacion(poblacion, cromosoma_secreto)
-        lista_mejores_candidatos.append(mejor_candidato)
-
-        if mejor_candidato[1][1] == 16: # fitness máximo 4*4 = 16
-            break
-
-        fitness_poblacion = medir_fitness(poblacion, cromosoma_secreto)
-        padres_seleccionados = selector_padres(fitness_poblacion)
-        hijos = crear_offspring(padres_seleccionados)
-        poblacion = crear_generacion(padres_seleccionados, hijos, TAMAÑO_POBLACION)
-
-        contador_generaciones += 1
-
-
-    return print(lista_mejores_candidatos)
 
 
 
@@ -58,5 +26,63 @@ def mejor_candidato_generacion(poblacion, cromosoma_secreto):
 
 
 
+def mostrar_intento(numero, cromosoma, pines):
+    # """
+    # Muestra el intennto por consola
+    # """
+    cromosoma_string = ' '.join(cromosoma)
+    pines_string = ' '.join(pines)
+    print('Intento' + str(numero) + ': ' + cromosoma_string + '|' + pines_string)
+
+
+
+
+def registro_generaciones(cromosoma_secreto):
+# """
+# Generamos la población inicial y dos variables que necesitaremos más adelante.
+# """
+    contador_generaciones = 0
+    poblacion = generar_poblacion_aleatoria(100)
+
+    print('Cromosoma secreto: ' + ' '.join(cromosoma_secreto))
+
+# """
+# Comenzamos el ciclo de generaciones.
+# """
+    while contador_generaciones < 14:
+
+        mejor_candidato = mejor_candidato_generacion(poblacion, cromosoma_secreto)
+        
+        intento_indice = contador_generaciones + 1
+        cromosoma = mejor_candidato[1][0]
+        pines = mejor_candidato[1][2]
+
+        mostrar_intento(intento_indice, cromosoma, pines)
+
+
+
+        if mejor_candidato[1][1] == 16: # fitness máximo 4*4 = 16
+            print('Código resuelto en ' + str(intento_indice) + '.')
+            return
+
+        fitness_poblacion = medir_fitness(poblacion, cromosoma_secreto)
+        padres_seleccionados = selector_padres(fitness_poblacion)
+        hijos = crear_offspring(padres_seleccionados)
+        poblacion = crear_generacion(padres_seleccionados, hijos, TAMAÑO_POBLACION)
+
+        contador_generaciones += 1
+
+
+    print('No se resolvió el código secreto en el número máximo de intentos.')
+
+
+
+
+
+
+
+
+
 if __name__ == "__main__":
-    registro_generaciones(cromosoma_secreto=introducir_cromosoma_secreto())
+    cromosoma_secreto = introducir_cromosoma_secreto()
+    registro_generaciones(cromosoma_secreto)
