2d092270befc3fcf0a5c90b0007e7f5c42f31718
adriceka <adrigonzalezgonzalez007@gmail.com>
2026-01-07 15:39:35 +0100

    build: :sparkles: Cambiar uso de la grafica
    Cambiar la gráfica mostrando ahoraeluso de colorespor generación

--- src/fitness_por_color.py
+++ src/fitness_por_color.py
@@ -4,16 +4,16 @@ from src.parametros_mastermind import EMOJIS_ALELOS
 def fitness_por_color(poblacion, cromosoma_secreto):
     colores = ["verde", "azul", "amarillo", "morado", "negro", "marrón"]
 
-    # Diccionario con el fitness acumulado de cada color
+    # Diccionario con el conteo de usos de cada color
     fitness_colores = {color: 0 for color in colores}
 
+    # Ya no necesitas fitness, pero mantenerlo no molesta
     fitness_poblacion = medir_fitness(poblacion, cromosoma_secreto)
 
-    for individuo, (cromosoma, fitness, _) in fitness_poblacion.items():
+    for individuo, (cromosoma, _, _) in fitness_poblacion.items():
         for emoji in cromosoma:
             for nombre_color, emoji_color in EMOJIS_ALELOS.items():
                 if emoji == emoji_color:
-                    fitness_colores[nombre_color] += fitness
+                    fitness_colores[nombre_color] += 1
 
     return fitness_colores
-
