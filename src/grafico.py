ad46fc0a926d663ef48b44f16ebc57c559690520
adriceka <adrigonzalezgonzalez007@gmail.com>
2026-01-07 15:40:43 +0100

    fix: :bug: Corregir colores de grafico
    Corregir el color erroneo que tenia el gráfico al ser mostrado

--- src/grafico.py
+++ src/grafico.py
@@ -3,12 +3,28 @@ import matplotlib.pyplot as plt
 def graficar_barras_fitness_por_generacion_y_color(generaciones, fitness_colores):
     plt.figure(figsize=(12, 6))
 
+    # Colores reales para cada línea
+    colores_plot = {
+        "verde": "green",
+        "azul": "blue",
+        "amarillo": "yellow",
+        "morado": "purple",
+        "negro": "black",
+        "marrón": "brown"
+    }
+
     for color, valores in fitness_colores.items():
-        plt.plot(generaciones, valores, marker='o', label=color)
+        plt.plot(
+            generaciones,
+            valores,
+            marker='o',
+            label=color,
+            color=colores_plot[color]
+        )
 
     plt.xlabel("Generación")
-    plt.ylabel("Fitness total por color")
-    plt.title("Evolución del fitness por color")
+    plt.ylabel("Uso total del color")
+    plt.title("Evolución del uso de colores por generación")
     plt.legend()
     plt.grid(True)
     plt.tight_layout()
